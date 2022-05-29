dba9248e83cc555bf7905b98dea72e6111beef6f
madeleinecmarie <madeleiinemarie@gmail.com>
2022-05-29 02:28:31 +0200

    a few small changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,50 +4,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    borderRadius: {
-      sm: "5px",
-    },
-    colors: {
-      white: "#ffffff",
-      black: "#1D282E",
-      yellow: "#F2D432",
-      orange: "#FEB95F",
-      gray: "rgba(29, 40, 46, 0.5)",
-    },
-    extend: {
-      lineHeight: {
-        "line-3": "3.4rem",
-      },
-      height: {
-        815: "815px",
-        447: "447px",
-        65: "65px",
-        55: "55px",
-        165: "165px",
-      },
-      width: {
-        28: "28%",
-        30: "30%",
-        160: "160px",
-        496: "496px",
-        253: "253px",
-        155: "155px",
-        144: "144px",
-        93: "93%",
-        396: "396px",
-        35: "35%",
-        943: "943px",
-        45: "45%",
-        229: "229px",
-        472: "472px",
-        700: "700px",
-      },
-      margin: {
-        88: "88px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
-
-// className="col-span-6 sm:col-span-3"
